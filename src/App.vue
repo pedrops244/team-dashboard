6656e97fa340e15cea6f3837fa89011d8ea814a7
Pedro Souza <pedrops244@hotmail.com>
2023-10-17 18:12:29 -0300

    exercising composition API and concepts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
 <template>
   <v-app>
     <v-app-bar>
-      <Header msg="Pedr11nnn" />
+      <TheHeader v-if="showHeader" />
     </v-app-bar>
-    <v-main class="mt-5 d-flex justify-center">
-      <h1>Hello World!</h1>
+
+    <v-main class="mt-5 d-flex flex-column">
+      <div v-show="showName">
+        <h1>Nome: {{ firstName }}</h1>
+        <h1>Sobrenome: {{ lastName }}</h1>
+      </div>
+      <div v-if="acessLevel === 'admin'">Admin</div>
+      <div v-else-if="acessLevel === 'marketing'">Marketing</div>
+      <div v-else>User</div>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import Header from '@/components/Header.vue';
+import TheHeader from '@/components/Header.vue';
+
+const props = defineProps({
+  showHeader: {
+    type: Boolean,
+    default: true,
+  },
+  showName: {
+    type: Boolean,
+    default: true,
+  },
+  firstName: {
+    type: String,
+    default: 'Pedr1n',
+  },
+  lastName: {
+    type: String,
+    default: 'Souzaa',
+  },
+  acessLevel: {
+    type: String,
+    default: '',
+  },
+});
 </script>
