c74117f080c72ec3048d74aeabda134ccb7c8328
Pedro Souza <pedrops244@hotmail.com>
2023-10-22 10:27:27 -0300

    using watch and watchEffect to simplify the logic

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,24 +1,21 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, computed, watch, watchEffect } from 'vue';
 
 const pessoa = ref({});
 const userCode = ref('');
-
-onMounted(async () => {
-  pessoa.value = await searchInformations(1);
-});
-
-const showBtn = computed(() => userCode.value > 0);
-
-const handleInformations = async () => {
-  pessoa.value = await searchInformations(userCode.value);
-};
 
 const searchInformations = async (code) => {
   const req = await fetch(`https://reqres.in/api/users/${code}`);
   const json = await req.json();
   return json.data;
 };
+
+watch(userCode, (novo) => {
+  if (novo <= 0) alert('Invalid code');
+});
+watchEffect(async () => {
+  pessoa.value = await searchInformations(userCode.value || 1);
+});
 </script>
 
 <template>
@@ -32,10 +29,7 @@ const searchInformations = async (code) => {
 
   <v-col cols="12" sm="6" md="4" lg="3">
     <v-card flat color="orange-darken-2" class="border-b">
-      <v-text-field v-model="userCode" type="number" min="0" max="12" label="User Code (1 - 12)"></v-text-field>
-      <v-card-actions>
-        <v-btn elevation="5" :disabled="!showBtn" @click="handleInformations">Buscar</v-btn>
-      </v-card-actions>
+      <v-text-field v-model="userCode" type="number" min="1" max="12" label="User Code (1 - 12)"></v-text-field>
     </v-card>
   </v-col>
 </template>
