3959b01622a146dd080576f2d9e617935cfd2eed
Pedro Souza <pedrops244@hotmail.com>
2023-10-25 19:04:03 -0300

    add persisted darkmode function

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,20 +1,25 @@
 <template>
   <v-app-bar color="primary" flat class="border-b">
-    <v-app-bar-title>Menu of app</v-app-bar-title>
-
-    <v-btn icon="mdi mdi-theme-light-dark" @click="toggleTheme"> </v-btn>
-    <v-btn prepend-icon="mdi-home"> Home </v-btn>
-    <v-btn prepend-icon="mdi-information-outline"> Sobre </v-btn>
-    <v-btn prepend-icon="mdi-login"> Login </v-btn>
+    <v-app-bar-title class="d-flex justify-center">
+      <v-btn icon="mdi mdi-theme-light-dark" @click="toggleTheme"> </v-btn>
+      <v-btn prepend-icon="mdi-home"> Home </v-btn>
+      <v-btn prepend-icon="mdi-information-outline"> Sobre </v-btn>
+      <v-btn prepend-icon="mdi-login"> Login </v-btn>
+    </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import { useTheme } from 'vuetify';
 
 const theme = useTheme();
+onMounted(() => {
+  theme.global.name.value = localStorage.getItem('darkTheme');
+});
 
 function toggleTheme() {
   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+  localStorage.setItem('darkTheme', theme.global.name.value.toString());
 }
 </script>
